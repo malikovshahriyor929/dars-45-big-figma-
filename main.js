6552098686a987a0be1b6b24e6cedad5588d85cb
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-02 22:24:51 +0500

    dsa

---
+++ main.js
@@ -0,0 +1,45 @@
+// // 1chi vazifa
+// let str = "javascriptisamazing"
+// console.log(str.slice(4,14))
+
+
+// // 2chi vazifa
+// let str = "  Find spaces here  ";
+// let res2 = str.trim().indexOf(" ")
+// res3= str.trim().lastIndexOf(" ")
+// console.log(res2+", "+res3);
+
+
+// // 3chi vazifa
+// let str  = "apple orange apple banana orange"
+// num= str
+// res= ""+" "
+// for(let i = 0 ;i<str.length;i++){
+//     console.log(str[i].split())
+//     if ((str[i]).split().join(" ") == str[i].split().join(" ")){
+//         res+=(str[i]);
+//     }
+//     else{
+//         res+=(str[i]);
+        
+//     }
+
+// }
+// console.log(res.split().join(" "))
+
+
+// // 4chi vazifa
+// let str = "apple123orange456banana789"
+// res= ""
+// for(let i = 0 ;i<str.length;i++){
+//     // console.log(Number()
+//     if (Number(str[i]) == str[i]){
+//         // console.log("man tengman")
+//     }
+//     else{
+//         res+=(str[i]);
+        
+//     }
+
+// }
+// console.log(res)
